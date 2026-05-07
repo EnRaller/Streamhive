3f86fba5a7de4e2be60d396df7ef96c8d6e5a58f
EnRaller <144103681+EnRaller@users.noreply.github.com>
2026-05-07 13:47:47 +0200

    index php gemaakt

---
+++ public/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Streamhive</title>
+    <link rel="stylesheet" href="../CSS/style.css?1">
+</head>
+<body>
+
+    <!-- Code voor de header -->
+    <div class="header">
+        <a href="#" class="logo">
+            <img src="../IMG/Streamhive.png" alt="Streamhive Logo">
+        </a>
+        <div class="header-right">
+            <a href="admin.php">Adminpagina</a>
+        </div>
+    </div>
+
+    <!-- Code voor de tekst in t midden-->
+    <div class="tekstmidden">
+        <h1>Welkom bij hotel Evernight</h1>
+    </div>
+
+
+</select>
+
+
+</form>
+</div>
+
+<a href="formulier.php">
+    <button>Nieuw formulier</button>
+</a>
+
+</body>
+</html>
